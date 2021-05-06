7fde13f4ab2f979426dbbebbc34957ffd096d3fb
AJ Downey <ajdowney@uw.edu>
2021-05-05 18:54:59 -0700

    Initial Commit with Test folder (can be deleted), and git ingnore basics

---
+++ TriviaMaze/src/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author ajdow
+ *
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stuba
+
+    }
+
+}
