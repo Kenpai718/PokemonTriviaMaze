45c82e19b3822a3da9557fe75e09e256002731a8
AJ Downey <ajdowney@uw.edu>
2021-05-10 15:36:52 -0700

    Created Base Classes for the Project.

---
+++ TriviaMaze/src/controller/PkmnTriviaMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author ajdow
+ *
+ */
+public class PkmnTriviaMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ TriviaMaze/src/view/MazeVisual.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package view;
+
+/**
+ * @author ajdow
+ *
+ */
+public class MazeVisual {
+
+}
